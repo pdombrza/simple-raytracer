72911bc1221bf8ea98752ae85d123e1e325722ff
Paweł Dombrzalski <01169161@pw.edu.pl>
2024-06-24 14:04:27 +0200

    Add basic camera calculations

--- sources/ray/ray.h
+++ sources/ray/ray.h
@@ -4,11 +4,11 @@
 
 class Ray {
 private:
-	glm::vec3 origin{ 0.f, 0.f, 0.f };
-	glm::vec3 direction{ 0.f, 0.f, 0.f };
+	glm::vec3 origin{ 0.0f, 0.0f, 0.0f };
+	glm::vec3 direction{ 0.0f, 0.0f, 0.0f };
 public:
 	constexpr Ray() = default;
-	Ray(const glm::vec3& direction, const glm::vec3& origin) : origin{ origin }, direction{ direction } {};
+	Ray(const glm::vec3& origin, const glm::vec3& direction) : origin{ origin }, direction{ glm::normalize(direction) } {};
 	glm::vec3 getOrigin() const;
 	glm::vec3 getDirection() const;
 	void setDirection(const glm::vec3& newDir);
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,55 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <algorithm>
 
 #include <glm/glm.hpp>
 
 #include <color.h>
+#include <ray.h>
 #include <tobmp.h>
 
-int main() {
-	const int imgWidth = 256;
-	const int imgHeight = 256;
+static glm::vec3 rayColor(const Ray& r) {
+	// gradient
+	glm::vec3 directionNormalized = r.getDirection();
+	float a = 0.5f * (directionNormalized.y + 1.0f);
+	return (1.0f - a) * glm::vec3(1.0f, 1.0f, 1.0f) + a * glm::vec3(0.5f, 0.7f, 1.0f);
+}
 
-	// ppm header
+int main() {
+	float aspectRatio = 16.0f / 9.0f;
+	int imgWidth = 400;
+
+	int imgHeight = (int) (imgWidth / aspectRatio);
+	imgHeight = std::max(1, imgHeight);
+
+
+	float focalLength = 1.0f;
+	float viewportHeight = 2.0f;
+	float viewportWidth = viewportHeight * ((float)imgWidth / imgHeight);
+	glm::vec3 cameraCenter( 0.0f, 0.0f, 0.0f );
+
+	glm::vec3 viewportU(viewportWidth, 0.0f, 0.0f );
+	glm::vec3 viewportV( 0.0f, -viewportHeight, 0.0f );
+
+	glm::vec3 pixelDeltaU = viewportU / (float)imgWidth;
+	glm::vec3 pixelDeltaV = viewportV / (float)imgHeight;
+
+	glm::vec3 viewportUpperLeft = cameraCenter - glm::vec3(0.0f, 0.0f, focalLength) - viewportU / 2.0f - viewportV / 2.0f;
+	glm::vec3 startPixelLoc = viewportUpperLeft + 0.5f * (pixelDeltaU + pixelDeltaV);
+		
+
 	std::stringstream stream;
-	//std::ofstream ppmFile(SOURCE_ROOT "/img.ppm");
+	std::ofstream ppmFile(SOURCE_ROOT "/img.ppm");
 	stream << "P3\n" << imgWidth << ' ' << imgHeight << "\n255\n";
 
 	for (size_t i = 0; i < imgHeight; i++) {
 		for (size_t j = 0; j < imgWidth; j++) {
-			glm::vec3 pixelColor = glm::vec3((float)i/(imgWidth-1), (float)j/(imgHeight-1), 0.f);
-			writeColorToStream(stream, pixelColor);
+			glm::vec3 pixelCenter = startPixelLoc + ((float)j * pixelDeltaU) + ((float)i * pixelDeltaV);
+			glm::vec3 rayDirection = pixelCenter - cameraCenter;
+			Ray r(cameraCenter, rayDirection);
+			glm::vec3 pxColor = rayColor(r);
+			writeColorToStream(stream, pxColor);
 		}
 	}
 
