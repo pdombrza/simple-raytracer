a02c8185a6a6c88dbd7ce56f65d4f7c125d1e756
Paweł Dombrzalski <01169161@pw.edu.pl>
2024-07-05 22:29:46 +0200

    Gamma correction

--- sources/color/color.cpp
+++ sources/color/color.cpp
@@ -1,11 +1,18 @@
 #include "color.h"
 
+float linearToGamma(float linearComponent) {
+	return (linearComponent > 0) ? std::sqrt(linearComponent) : 0.0f;
+}
+
 void writeColorToStream(std::stringstream& out, const glm::vec3& pixelColor) {
 	// writes color to stringstream
+	float r = linearToGamma(pixelColor.x);
+	float g = linearToGamma(pixelColor.y);
+	float b = linearToGamma(pixelColor.z);
 
-	int rTranslated = (int)(256 * std::clamp(pixelColor.x, 0.0f, 0.999f));
-	int gTranslated = (int)(256 * std::clamp(pixelColor.y, 0.0f, 0.999f));
-	int bTranslated = (int)(256 * std::clamp(pixelColor.z, 0.0f, 0.999f));
+	int rTranslated = (int)(256 * std::clamp(r, 0.0f, 0.999f));
+	int gTranslated = (int)(256 * std::clamp(g, 0.0f, 0.999f));
+	int bTranslated = (int)(256 * std::clamp(b, 0.0f, 0.999f));
 
 	out << rTranslated << ' ' << gTranslated << ' ' << bTranslated << '\n';
 }
--- sources/color/color.h
+++ sources/color/color.h
@@ -5,4 +5,5 @@
 #include <glm/glm.hpp>
 
 
+float linearToGamma(float linearComponent);
 void writeColorToStream(std::stringstream& out, const glm::vec3& pixelColor);
