3a7a071a36f6095c7b85ce88528d0dc55b70e576
pdombrza <pawel.dombrzalski@gmail.com>
2025-08-13 17:33:09 +0200

    Add cuda vec3 class

---
+++ sources/cuvec/cuvec.cu
@@ -0,0 +1,111 @@
+#include "cuvec.h"
+
+__host__ __device__ cu::vec3::vec3(float x, float y, float z) {
+	data[0] = x;
+	data[1] = y;
+	data[2] = z;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator+=(const cu::vec3& v) {
+	data[0] += v.x();
+	data[1] += v.y();
+	data[2] += v.z();
+	return *this;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator-=(const cu::vec3& v) {
+	data[0] -= v.x();
+	data[1] -= v.y();
+	data[2] -= v.z();
+	return *this;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator*=(const cu::vec3& v) {
+	data[0] *= v.x();
+	data[1] *= v.y();
+	data[2] *= v.z();
+	return *this;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator/=(const cu::vec3& v) {
+	data[0] /= v.x();
+	data[1] /= v.y();
+	data[2] /= v.z();
+	return *this;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator*=(float t) {
+	data[0] *= t;
+	data[1] *= t;
+	data[2] *= t;
+	return *this;
+}
+
+__host__ __device__ cu::vec3& cu::vec3::operator/=(float t) {
+	float k = 1.0 / t;
+	data[0] *= k;
+	data[1] *= k;
+	data[2] *= k;
+	return *this;
+}
+
+__host__ __device__ float cu::vec3::length() const {
+	return sqrtf(dot(*this, *this));
+}
+
+__host__ __device__ float cu::vec3::length_squared() const {
+	return dot(*this, *this);
+}
+
+__host__ __device__ void cu::vec3::make_unit_vector() {
+	data[0] /= length();
+	data[1] /= length();
+	data[2] /= length();
+}
+
+__host__ __device__ cu::vec3 operator+(const cu::vec3& v1, const cu::vec3& v2) {
+	return cu::vec3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
+}
+
+__host__ __device__ cu::vec3 operator-(const cu::vec3& v1, const cu::vec3& v2) {
+	return cu::vec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
+}
+
+__host__ __device__ cu::vec3 operator*(const cu::vec3& v1, const cu::vec3& v2) {
+	return cu::vec3(v1.x() * v2.x(), v1.y() * v2.y(), v1.z() * v2.z());
+}
+
+__host__ __device__ cu::vec3 operator/(const cu::vec3& v1, const cu::vec3& v2) {
+	return cu::vec3(v1.x() / v2.x(), v1.y() / v2.y(), v1.z() / v2.z());
+}
+
+__host__ __device__ cu::vec3 operator*(float t, const cu::vec3& v) {
+	return cu::vec3(t * v.x(), t * v.y(), t * v.z());
+}
+
+__host__ __device__ cu::vec3 operator/(const cu::vec3& v, float t) {
+	return cu::vec3(v.x() / t, v.y() / t, v.z() / t);
+}
+
+__host__ __device__ float dot(const cu::vec3& v1, const cu::vec3& v2) {
+	return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+}
+
+__host__ __device__ cu::vec3 cross(const cu::vec3& v1, const cu::vec3& v2) {
+	return cu::vec3{ v1.y() * v2.z() - v1.z() * v2.y(), v1.z() * v2.x() - v1.x() * v2.z(), v1.x() * v2.y() - v1.y() * v2.x() };
+}
+
+inline std::istream& operator>>(std::istream& is, cu::vec3& t) {
+	float x, y, z;
+	is >> x >> y >> z;
+	t = cu::vec3(x, y, z);
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const cu::vec3& t) {
+	float x = t.x();
+	float y = t.y();
+	float z = t.z();
+	os << x << " " << y << " " << z;
+	return os;
+}
---
+++ sources/cuvec/cuvec.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <array>
+
+#include "cudaray.h"
+
+
+namespace cu {
+	class vec3 {
+	protected:
+		float data[3];
+	public:
+		__host__ __device__ vec3() : data{ 0.0f, 0.0f, 0.0f } {}
+		__host__ __device__ vec3(float x, float y, float z);
+		__host__ __device__ inline float x() const { return data[0]; }
+		__host__ __device__ inline float y() const { return data[1]; }
+		__host__ __device__ inline float z() const { return data[2]; }
+		__host__ __device__ inline float r() const { return x(); };
+		__host__ __device__ inline float g() const { return y(); };
+		__host__ __device__ inline float b() const { return z(); };
+
+		__host__ __device__ vec3 operator-() { return vec3(-data[0], -data[1], -data[2]); };
+		__host__ __device__ float operator[](int i) const { return data[i]; };
+		__host__ __device__ float& operator[](int i) { return data[i]; }
+
+		__host__ __device__ vec3& operator+=(const vec3& v);
+		__host__ __device__ vec3& operator-=(const vec3& v);
+		__host__ __device__ vec3& operator*=(const vec3& v);
+		__host__ __device__ vec3& operator/=(const vec3& v);
+
+		__host__ __device__ vec3& operator*=(float t);
+		__host__ __device__ vec3& operator/=(float t);
+
+		__host__ __device__ float length() const;
+		__host__ __device__ float length_squared() const;
+
+		__host__ __device__ void make_unit_vector();
+	};
+}
+
+__host__ __device__ cu::vec3 operator+(const cu::vec3& v1, const cu::vec3& v2);
+__host__ __device__ cu::vec3 operator-(const cu::vec3& v1, const cu::vec3& v2);
+__host__ __device__ cu::vec3 operator*(const cu::vec3& v1, const cu::vec3& v2);
+__host__ __device__ cu::vec3 operator/(const cu::vec3& v1, const cu::vec3& v2);
+
+__host__ __device__ cu::vec3 operator*(float t, const cu::vec3& v);
+__host__ __device__ cu::vec3 operator/(const cu::vec3& v, float t);
+
+__host__ __device__ float dot(const cu::vec3& v1, const cu::vec3& v2);
+__host__ __device__ cu::vec3 cross(const cu::vec3& v1, const cu::vec3& v2);
+
+inline std::istream& operator>>(std::istream& is, cu::vec3& t);
+inline std::ostream& operator<<(std::ostream& os, const cu::vec3& t);
